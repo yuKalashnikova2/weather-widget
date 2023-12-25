d07239c675c7818ca9bc13b5a7b3e3a3b9263a47
yuKalasnikova2 <pmalian@list.ru>
2023-12-25 16:49:59 +0300

    refactor(styles)

--- src/components/CardWeather.vue
+++ src/components/CardWeather.vue
@@ -5,7 +5,7 @@ export default {
     return {
       dateNow: format(new Date(), 'dd/MM'),
       dayWeek: new Date(),
-      dataTemp: this.dataWeather?.main.temp, 
+      dataTemp: this.dataWeather?.main.temp,
     }
   },
   props: {
@@ -16,32 +16,52 @@ export default {
   },
   methods: {
     styleBackground(obj) {
-      return Math.round(obj) < 7 ? '' : 'card__weather_background-yellow' ? Math.round(obj) > 15 && Math.round(obj) < 23 : 'card__weather_background-red'
-    }
+      console.log(obj)
+      if (Math.round(obj) < 7) {
+        return ''
+      } else if (Math.round(obj) > 15 && Math.round(obj) < 23) {
+        return 'card__weather_background-red'
+      } else {
+        return 'card__weather_background-yellow'
+      }
+    },
   },
 }
 </script>
 
 <template>
-  <div :class="['card__weather', styleBackground(Math.round(dataWeather?.main.temp))]">
+  <div
+    :class="[
+      'card__weather',
+      styleBackground(Math.round(dataWeather?.main.temp)),
+    ]"
+  >
     <div class="card__weather-title">
       <h2 class="card__weather-title-name">
         {{ Math.round(dataWeather?.main.temp) }}°
       </h2>
-      <span class="card__weather-title-city">{{ dataWeather?.name ? dataWeather?.name : 'City undefined' }}</span>
+      <span class="card__weather-title-city">{{
+        dataWeather?.name ? dataWeather?.name : 'City undefined'
+      }}</span>
     </div>
     <div class="card__weather-info">
-      <div class="card__weather-info-week">{{ dayWeek.toLocaleString('en', {weekday: 'long'}) }}, {{ dateNow }}</div>
+      <div class="card__weather-info-week">
+        {{ dayWeek.toLocaleString('en', { weekday: 'long' }) }}, {{ dateNow }}
+      </div>
 
       <div class="card__weather-info-descr">
         <div>
           <span>{{ dataWeather?.weather[0].main }}</span>
-          <span>{{ Math.round(dataWeather?.main.temp_min) }}° /
-            {{ Math.round(dataWeather?.main.temp_max) }}°</span>
+          <span
+            >{{ Math.round(dataWeather?.main.temp_min) }}° /
+            {{ Math.round(dataWeather?.main.temp_max) }}°</span
+          >
         </div>
         <div class="card__weather-info-icon">
-          
-          <img :src="`https://openweathermap.org/img/wn/${dataWeather?.weather[0].icon}.png`" :alt="dataWeather?.weather[0].icon" />
+          <img
+            :src="`https://openweathermap.org/img/wn/${dataWeather?.weather[0].icon}.png`"
+            :alt="dataWeather?.weather[0].icon"
+          />
         </div>
       </div>
     </div>
@@ -71,10 +91,29 @@ export default {
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.25);
   overflow: hidden;
   &_background-red {
-    background: linear-gradient(322deg, rgba(249, 57, 57, 0.20) 13.99%, rgba(255, 98, 98, 0.20) 104.25%), linear-gradient(273deg, #F94545 -7%, rgba(255, 80, 80, 0.65) 102.49%), url('https://www.erv.ru/upload/medialibrary/587/ww7gich3fbgmb19hqv6z82r8ithpch0x/%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D0%BD%D1%8B%D0%B9%20%D0%9A%D0%BE%D0%BB%D0%B8%D0%B7%D0%B5%D0%B9%20%D0%BD%D0%B0%20%D0%B2%D0%BE%D1%81%D1%85%D0%BE%D0%B4%D0%B5%20%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B0,%20%D0%A0%D0%B8%D0%BC,%20%D0%98%D1%82%D0%B0%D0%BB%D0%B8%D1%8F.jpg'), lightgray -3px -19.573px / 102.469% 100% no-repeat;
+    background: linear-gradient(
+        322deg,
+        rgba(249, 57, 57, 0.2) 13.99%,
+        rgba(255, 98, 98, 0.2) 104.25%
+      ),
+      linear-gradient(273deg, #f94545 -7%, rgba(255, 80, 80, 0.65) 102.49%),
+      url('https://www.erv.ru/upload/medialibrary/587/ww7gich3fbgmb19hqv6z82r8ithpch0x/%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D0%BD%D1%8B%D0%B9%20%D0%9A%D0%BE%D0%BB%D0%B8%D0%B7%D0%B5%D0%B9%20%D0%BD%D0%B0%20%D0%B2%D0%BE%D1%81%D1%85%D0%BE%D0%B4%D0%B5%20%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B0,%20%D0%A0%D0%B8%D0%BC,%20%D0%98%D1%82%D0%B0%D0%BB%D0%B8%D1%8F.jpg'),
+      lightgray -3px -19.573px / 102.469% 100% no-repeat;
   }
   &_background-yellow {
-    background: linear-gradient(325deg, rgba(239, 188, 56, 0.14) 14.05%, rgba(250, 186, 24, 0.20) 100%), linear-gradient(271deg, #FDD368 -4.18%, #F8B70F -4.17%, rgba(222, 194, 123, 0.60) 110.03%), url('https://www.erv.ru/upload/medialibrary/587/ww7gich3fbgmb19hqv6z82r8ithpch0x/%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D0%BD%D1%8B%D0%B9%20%D0%9A%D0%BE%D0%BB%D0%B8%D0%B7%D0%B5%D0%B9%20%D0%BD%D0%B0%20%D0%B2%D0%BE%D1%81%D1%85%D0%BE%D0%B4%D0%B5%20%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B0,%20%D0%A0%D0%B8%D0%BC,%20%D0%98%D1%82%D0%B0%D0%BB%D0%B8%D1%8F.jpg'), lightgray 50% / cover no-repeat;
+    background: linear-gradient(
+        325deg,
+        rgba(239, 188, 56, 0.14) 14.05%,
+        rgba(250, 186, 24, 0.2) 100%
+      ),
+      linear-gradient(
+        271deg,
+        #fdd368 -4.18%,
+        #f8b70f -4.17%,
+        rgba(222, 194, 123, 0.6) 110.03%
+      ),
+      url('https://www.erv.ru/upload/medialibrary/587/ww7gich3fbgmb19hqv6z82r8ithpch0x/%D0%9B%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D0%BD%D1%8B%D0%B9%20%D0%9A%D0%BE%D0%BB%D0%B8%D0%B7%D0%B5%D0%B9%20%D0%BD%D0%B0%20%D0%B2%D0%BE%D1%81%D1%85%D0%BE%D0%B4%D0%B5%20%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B0,%20%D0%A0%D0%B8%D0%BC,%20%D0%98%D1%82%D0%B0%D0%BB%D0%B8%D1%8F.jpg'),
+      lightgray 50% / cover no-repeat;
   }
   &-title {
     flex-grow: 1;
